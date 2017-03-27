fdb9c10fd64835d813db3c0f3a48141ab36a40f6
Derek T <fbfb1694@opayq.com>
2017-03-27 22:04:22 +0000

    redesign commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Derek Thurber Personal Porfolio Website
+=========
+
+All rights reserved.
+
+Special thanks to CodyHouse for animation effects and web components.
+
+[CodyHouse](http://codyhouse.co/)
+ 
+[Terms](http://codyhouse.co/terms/)
--- js/main.js
+++ js/main.js
@@ -1,198 +1,264 @@
-jQuery(document).ready(function(){
-	var intro = $('.cd-intro-block'),
-		projectsContainer = $('.cd-projects-wrapper'),
-		projectsSlider = projectsContainer.children('.cd-slider'),
-		singleProjectContent = $('.cd-project-content'),
-		sliderNav = $('.cd-slider-navigation');
-
-	var resizing = false;
+jQuery(document).ready(function($){
+	//set animation timing
+	var animationDelay = 2500,
+		//loading bar effect
+		barAnimationDelay = 3800,
+		barWaiting = barAnimationDelay - 3000, //3000 is the duration of the transition on the loading bar - set in the scss/css file
+		//letters effect
+		lettersDelay = 50,
+		//type effect
+		typeLettersDelay = 150,
+		selectionDuration = 500,
+		typeAnimationDelay = selectionDuration + 800,
+		//clip effect 
+		revealDuration = 600,
+		revealAnimationDelay = 1500;
 	
-	//if on desktop - set a width for the projectsSlider element
-	setSliderContainer();
-	$(window).on('resize', function(){
-		//on resize - update projectsSlider width and translate value
-		if( !resizing ) {
-			(!window.requestAnimationFrame) ? setSliderContainer() : window.requestAnimationFrame(setSliderContainer);
-			resizing = true;
-		}
-	});
+	initHeadline();
+	
 
-	//show the projects slider if user clicks the show-projects button
-	intro.on('click', 'a[data-action="show-projects"]', function(event) {
-		event.preventDefault();
-		intro.addClass('projects-visible');
-		projectsContainer.addClass('projects-visible');
-		//animate single project - entrance animation
-		setTimeout(function(){
-			showProjectPreview(projectsSlider.children('li').eq(0));
-		}, 200);
-	});
-
-	intro.on('click', function(event) {
-		//projects slider is visible - hide slider and show the intro panel
-		if( intro.hasClass('projects-visible') && !$(event.target).is('a[data-action="show-projects"]') ) {
-			intro.removeClass('projects-visible');
-			projectsContainer.removeClass('projects-visible');
-		}
-	});
-
-	// //select a single project - open project-content panel
-	// projectsContainer.on('click', '.cd-slider a', function(event) {
-	// 	var mq = checkMQ();
-	// 	event.preventDefault();
-	// 	if( $(this).parent('li').next('li').is('.current') && (mq == 'desktop') ) {
-	// 		prevSides(projectsSlider);
-	// 	} else if ( $(this).parent('li').prev('li').prev('li').prev('li').is('.current')  && (mq == 'desktop') ) {
-	// 		nextSides(projectsSlider);
-	// 	} else {
-	// 		singleProjectContent.addClass('is-visible');
-	// 	}
-	// });
-
-	// //close single project content
-	// singleProjectContent.on('click', '.close', function(event){
-	// 	event.preventDefault();
-	// 	singleProjectContent.removeClass('is-visible');
-	// });
-
-	//go to next/pre slide - clicking on the next/prev arrow
-	sliderNav.on('click', '.next', function(){
-		nextSides(projectsSlider);
-	});
-	sliderNav.on('click', '.prev', function(){
-		prevSides(projectsSlider);
-	});
-
-	//go to next/pre slide - keyboard navigation
-	$(document).keyup(function(event){
-		var mq = checkMQ();
-		if(event.which=='37' &&  intro.hasClass('projects-visible') && !(sliderNav.find('.prev').hasClass('inactive')) && (mq == 'desktop') ) {
-			prevSides(projectsSlider);
-		} else if( event.which=='39' &&  intro.hasClass('projects-visible') && !(sliderNav.find('.next').hasClass('inactive')) && (mq == 'desktop') ) {
-			nextSides(projectsSlider);
-		} else if(event.which=='27' && singleProjectContent.hasClass('is-visible')) {
-			singleProjectContent.removeClass('is-visible');
-		}
-	});
-
-	projectsSlider.on('swipeleft', function(){
-		var mq = checkMQ();
-		if( !(sliderNav.find('.next').hasClass('inactive')) && (mq == 'desktop') ) nextSides(projectsSlider);
-	});
-
-	projectsSlider.on('swiperight', function(){
-		var mq = checkMQ();
-		if ( !(sliderNav.find('.prev').hasClass('inactive')) && (mq == 'desktop') ) prevSides(projectsSlider);
-	});
-
-	function showProjectPreview(project) {
-		if(project.length > 0 ) {
-			setTimeout(function(){
-				project.addClass('slides-in');
-				showProjectPreview(project.next());
-			}, 50);
-		}
+	function initHeadline() {
+		//insert <i> element for each letter of a changing word
+		singleLetters($('.cd-headline.letters').find('b'));
+		//initialise headline animation
+		animateHeadline($('.cd-headline'));
 	}
 
-	function checkMQ() {
-		//check if mobile or desktop device
-		return window.getComputedStyle(document.querySelector('.cd-projects-wrapper'), '::before').getPropertyValue('content').replace(/'/g, "").replace(/"/g, "");
-	}
-
-	function setSliderContainer() {
-		var mq = checkMQ();
-		if(mq == 'desktop') {
-			var	slides = projectsSlider.children('li'),
-				slideWidth = slides.eq(0).width(),
-				marginLeft = Number(projectsSlider.children('li').eq(1).css('margin-left').replace('px', '')),
-				sliderWidth = ( slideWidth + marginLeft )*( slides.length + 1 ) + 'px',
-				slideCurrentIndex = projectsSlider.children('li.current').index();
-			projectsSlider.css('width', sliderWidth);
-			( slideCurrentIndex != 0 ) && setTranslateValue(projectsSlider, (  slideCurrentIndex * (slideWidth + marginLeft) + 'px'));
-		} else {
-			projectsSlider.css('width', '');
-			setTranslateValue(projectsSlider, 0);
-		}
-		resizing = false;
-	}
-
-	function nextSides(slider) {
-		var actual = slider.children('.current'),
-			index = actual.index(),
-			following = actual.nextAll('li').length,
-			width = actual.width(),
-			marginLeft = Number(slider.children('li').eq(1).css('margin-left').replace('px', ''));
-
-		index = (following > 4 ) ? index + 3 : index + following - 2;
-		//calculate the translate value of the slider container
-		translate = index * (width + marginLeft) + 'px';
-
-		slider.addClass('next');
-		setTranslateValue(slider, translate);
-		slider.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-			updateSlider('next', actual, slider, following);
-		});
-
-		if( $('.no-csstransitions').length > 0 ) updateSlider('next', actual, slider, following);
-	}
-
-	function prevSides(slider) {
-		var actual = slider.children('.previous'),
-			index = actual.index(),
-			width = actual.width(),
-			marginLeft = Number(slider.children('li').eq(1).css('margin-left').replace('px', ''));
-
-		translate = index * (width + marginLeft) + 'px';
-
-		slider.addClass('prev');
-		setTranslateValue(slider, translate);
-		slider.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-			updateSlider('prev', actual, slider);
-		});
-
-		if( $('.no-csstransitions').length > 0 ) updateSlider('prev', actual, slider);
-	}
-
-	function updateSlider(direction, actual, slider, numerFollowing) {
-		if( direction == 'next' ) {
-			
-			slider.removeClass('next').find('.previous').removeClass('previous');
-			actual.removeClass('current');
-			if( numerFollowing > 4 ) {
-				actual.addClass('previous').next('li').next('li').next('li').addClass('current');
-			} else if ( numerFollowing == 4 ) {
-				actual.next('li').next('li').addClass('current').prev('li').prev('li').addClass('previous');
-			} else {
-				actual.next('li').addClass('current').end().addClass('previous');
+	function singleLetters($words) {
+		$words.each(function(){
+			var word = $(this),
+				letters = word.text().split(''),
+				selected = word.hasClass('is-visible');
+			for (i in letters) {
+				if(word.parents('.rotate-2').length > 0) letters[i] = '<em>' + letters[i] + '</em>';
+				letters[i] = (selected) ? '<i class="in">' + letters[i] + '</i>': '<i>' + letters[i] + '</i>';
 			}
-		} else {
+		    var newLetters = letters.join('');
+		    word.html(newLetters).css('opacity', 1);
+		});
+	}
+
+	function animateHeadline($headlines) {
+		var duration = animationDelay;
+		$headlines.each(function(){
+			var headline = $(this);
 			
-			slider.removeClass('prev').find('.current').removeClass('current');
-			actual.removeClass('previous').addClass('current');
-			if(actual.prevAll('li').length > 2 ) {
-				actual.prev('li').prev('li').prev('li').addClass('previous');
-			} else {
-				( !slider.children('li').eq(0).hasClass('current') ) && slider.children('li').eq(0).addClass('previous');
-			}
-		}
+			if(headline.hasClass('loading-bar')) {
+				duration = barAnimationDelay;
+				setTimeout(function(){ headline.find('.cd-words-wrapper').addClass('is-loading') }, barWaiting);
+			} else if (headline.hasClass('clip')){
+				var spanWrapper = headline.find('.cd-words-wrapper'),
+					newWidth = spanWrapper.width() + 10
+				spanWrapper.css('width', newWidth);
+			} else if (!headline.hasClass('type') ) {
+				//assign to .cd-words-wrapper the width of its longest word
+				var words = headline.find('.cd-words-wrapper b'),
+					width = 0;
+				words.each(function(){
+					var wordWidth = $(this).width();
+				    if (wordWidth > width) width = wordWidth;
+				});
+				headline.find('.cd-words-wrapper').css('width', width);
+			};
+
+			//trigger animation
+			setTimeout(function(){ hideWord( headline.find('.is-visible').eq(0) ) }, duration);
+		});
+	}
+
+	function hideWord($word) {
+		var nextWord = takeNext($word);
 		
-		updateNavigation();
+		if($word.parents('.cd-headline').hasClass('type')) {
+			var parentSpan = $word.parent('.cd-words-wrapper');
+			parentSpan.addClass('selected').removeClass('waiting');	
+			setTimeout(function(){ 
+				parentSpan.removeClass('selected'); 
+				$word.removeClass('is-visible').addClass('is-hidden').children('i').removeClass('in').addClass('out');
+			}, selectionDuration);
+			setTimeout(function(){ showWord(nextWord, typeLettersDelay) }, typeAnimationDelay);
+		
+		} else if($word.parents('.cd-headline').hasClass('letters')) {
+			var bool = ($word.children('i').length >= nextWord.children('i').length) ? true : false;
+			hideLetter($word.find('i').eq(0), $word, bool, lettersDelay);
+			showLetter(nextWord.find('i').eq(0), nextWord, bool, lettersDelay);
+
+		}  else if($word.parents('.cd-headline').hasClass('clip')) {
+			$word.parents('.cd-words-wrapper').animate({ width : '2px' }, revealDuration, function(){
+				switchWord($word, nextWord);
+				showWord(nextWord);
+			});
+
+		} else if ($word.parents('.cd-headline').hasClass('loading-bar')){
+			$word.parents('.cd-words-wrapper').removeClass('is-loading');
+			switchWord($word, nextWord);
+			setTimeout(function(){ hideWord(nextWord) }, barAnimationDelay);
+			setTimeout(function(){ $word.parents('.cd-words-wrapper').addClass('is-loading') }, barWaiting);
+
+		} else {
+			switchWord($word, nextWord);
+			setTimeout(function(){ hideWord(nextWord) }, animationDelay);
+		}
 	}
 
-	function updateNavigation() {
-		//update visibility of next/prev buttons according to the visible slides
-		var current = projectsContainer.find('li.current');
-		(current.is(':first-child')) ? sliderNav.find('.prev').addClass('inactive') : sliderNav.find('.prev').removeClass('inactive');
-		(current.nextAll('li').length < 3 ) ? sliderNav.find('.next').addClass('inactive') : sliderNav.find('.next').removeClass('inactive');
+	function showWord($word, $duration) {
+		if($word.parents('.cd-headline').hasClass('type')) {
+			showLetter($word.find('i').eq(0), $word, false, $duration);
+			$word.addClass('is-visible').removeClass('is-hidden');
+
+		}  else if($word.parents('.cd-headline').hasClass('clip')) {
+			$word.parents('.cd-words-wrapper').animate({ 'width' : $word.width() + 10 }, revealDuration, function(){ 
+				setTimeout(function(){ hideWord($word) }, revealAnimationDelay); 
+			});
+		}
 	}
 
-	function setTranslateValue(item, translate) {
-		item.css({
-		    '-moz-transform': 'translateX(-' + translate + ')',
-		    '-webkit-transform': 'translateX(-' + translate + ')',
-			'-ms-transform': 'translateX(-' + translate + ')',
-			'-o-transform': 'translateX(-' + translate + ')',
-			'transform': 'translateX(-' + translate + ')',
+	function hideLetter($letter, $word, $bool, $duration) {
+		$letter.removeClass('in').addClass('out');
+		
+		if(!$letter.is(':last-child')) {
+		 	setTimeout(function(){ hideLetter($letter.next(), $word, $bool, $duration); }, $duration);  
+		} else if($bool) { 
+		 	setTimeout(function(){ hideWord(takeNext($word)) }, animationDelay);
+		}
+
+		if($letter.is(':last-child') && $('html').hasClass('no-csstransitions')) {
+			var nextWord = takeNext($word);
+			switchWord($word, nextWord);
+		} 
+	}
+
+	function showLetter($letter, $word, $bool, $duration) {
+		$letter.addClass('in').removeClass('out');
+		
+		if(!$letter.is(':last-child')) { 
+			setTimeout(function(){ showLetter($letter.next(), $word, $bool, $duration); }, $duration); 
+		} else { 
+			if($word.parents('.cd-headline').hasClass('type')) { setTimeout(function(){ $word.parents('.cd-words-wrapper').addClass('waiting'); }, 200);}
+			if(!$bool) { setTimeout(function(){ hideWord($word) }, animationDelay) }
+		}
+	}
+
+	function takeNext($word) {
+		return (!$word.is(':last-child')) ? $word.next() : $word.parent().children().eq(0);
+	}
+
+	function takePrev($word) {
+		return (!$word.is(':first-child')) ? $word.prev() : $word.parent().children().last();
+	}
+
+	function switchWord($oldWord, $newWord) {
+		$oldWord.removeClass('is-visible').addClass('is-hidden');
+		$newWord.removeClass('is-hidden').addClass('is-visible');
+	}
+    var modalTriggerBts = $('a[data-type="cd-modal-trigger"]'),
+		coverLayer = $('.cd-cover-layer');
+	
+	/*
+		convert a cubic bezier value to a custom mina easing
+		http://stackoverflow.com/questions/25265197/how-to-convert-a-cubic-bezier-value-to-a-custom-mina-easing-snap-svg
+	*/
+	var duration = 600,
+		epsilon = (1000 / 60 / duration) / 4,
+		firstCustomMinaAnimation = bezier(.63,.35,.48,.92, epsilon);
+
+	modalTriggerBts.each(function(){
+		initModal($(this));
+	});
+
+	function initModal(modalTrigger) {
+		var modalTriggerId =  modalTrigger.attr('id'),
+			modal = $('.cd-modal[data-modal="'+ modalTriggerId +'"]'),
+			svgCoverLayer = modal.children('.cd-svg-bg'),
+			paths = svgCoverLayer.find('path'),
+			pathsArray = [];
+		//store Snap objects
+		pathsArray[0] = Snap('#'+paths.eq(0).attr('id')),
+		pathsArray[1] = Snap('#'+paths.eq(1).attr('id')),
+		pathsArray[2] = Snap('#'+paths.eq(2).attr('id'));
+
+		//store path 'd' attribute values	
+		var pathSteps = [];
+		pathSteps[0] = svgCoverLayer.data('step1');
+		pathSteps[1] = svgCoverLayer.data('step2');
+		pathSteps[2] = svgCoverLayer.data('step3');
+		pathSteps[3] = svgCoverLayer.data('step4');
+		pathSteps[4] = svgCoverLayer.data('step5');
+		pathSteps[5] = svgCoverLayer.data('step6');
+		
+		//open modal window
+		modalTrigger.on('click', function(event){
+			event.preventDefault();
+			modal.addClass('modal-is-visible');
+			coverLayer.addClass('modal-is-visible');
+			animateModal(pathsArray, pathSteps, duration, 'open');
+		});
+
+		//close modal window
+		modal.on('click', '.modal-close', function(event){
+			event.preventDefault();
+			modal.removeClass('modal-is-visible');
+			coverLayer.removeClass('modal-is-visible');
+			animateModal(pathsArray, pathSteps, duration, 'close');
 		});
 	}
+
+	function animateModal(paths, pathSteps, duration, animationType) {
+		var path1 = ( animationType == 'open' ) ? pathSteps[1] : pathSteps[0],
+			path2 = ( animationType == 'open' ) ? pathSteps[3] : pathSteps[2],
+			path3 = ( animationType == 'open' ) ? pathSteps[5] : pathSteps[4];
+		paths[0].animate({'d': path1}, duration, firstCustomMinaAnimation);
+		paths[1].animate({'d': path2}, duration, firstCustomMinaAnimation);
+		paths[2].animate({'d': path3}, duration, firstCustomMinaAnimation);
+	}
+
+	function bezier(x1, y1, x2, y2, epsilon){
+		//https://github.com/arian/cubic-bezier
+		var curveX = function(t){
+			var v = 1 - t;
+			return 3 * v * v * t * x1 + 3 * v * t * t * x2 + t * t * t;
+		};
+
+		var curveY = function(t){
+			var v = 1 - t;
+			return 3 * v * v * t * y1 + 3 * v * t * t * y2 + t * t * t;
+		};
+
+		var derivativeCurveX = function(t){
+			var v = 1 - t;
+			return 3 * (2 * (t - 1) * t + v * v) * x1 + 3 * (- t * t * t + 2 * v * t) * x2;
+		};
+
+		return function(t){
+
+			var x = t, t0, t1, t2, x2, d2, i;
+
+			// First try a few iterations of Newton's method -- normally very fast.
+			for (t2 = x, i = 0; i < 8; i++){
+				x2 = curveX(t2) - x;
+				if (Math.abs(x2) < epsilon) return curveY(t2);
+				d2 = derivativeCurveX(t2);
+				if (Math.abs(d2) < 1e-6) break;
+				t2 = t2 - x2 / d2;
+			}
+
+			t0 = 0, t1 = 1, t2 = x;
+
+			if (t2 < t0) return curveY(t0);
+			if (t2 > t1) return curveY(t1);
+
+			// Fallback to the bisection method for reliability.
+			while (t0 < t1){
+				x2 = curveX(t2);
+				if (Math.abs(x2 - x) < epsilon) return curveY(t2);
+				if (x > x2) t0 = t2;
+				else t1 = t2;
+				t2 = (t1 - t0) * .5 + t0;
+			}
+
+			// Failure
+			return curveY(t2);
+
+		};
+	};
 });
